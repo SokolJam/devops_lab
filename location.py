bc6e7eaaf6d8b40a78d3225ac61f74a5e58152dc
Yauheni Sokal <sokoljam@gmail.com>
2018-07-17 13:17:38 +0300

    homework6

---
+++ location.py
@@ -0,0 +1,31 @@
+import sys
+import os
+import pip
+import pkg_resources
+import yaml
+import json
+
+
+result = {'Python version': sys.version,
+          'Virtual environment': os.getenv('PYENV_VERSION'),
+          'Python executable location': sys.executable,
+          'pip location': pip.__file__,
+          'PYTHONPATH': sys.path,
+          'Installed packages': [],
+          }
+
+for package in pkg_resources.working_set:
+    result['Installed packages'].append(str(package))
+
+for i in sys.path:
+    if 'site-packages' in i:
+        result['Site-packages location'] = i
+
+for element in result:
+    print(element, ':', result[element])
+
+with open('output_06.json', 'w') as j_file:
+    json.dump(result, j_file)
+
+with open('output_06.yml', 'w') as y_file:
+    yaml.dump(result, y_file)
